b5cfdf2eed9dd87cae48b46fa26f2643538eafaf
Jared Williams <jaredlukewilliams@gmail.com>
2026-05-26 09:58:01 +0200

    Small fixes

--- slack/server.js
+++ slack/server.js
@@ -24,6 +24,7 @@ import {
   USE_SOCKET_MODE,
   PORT,
 } from './config.js';
+import 'dotenv/config';
 
 // ---------------------------------------------------------------------------
 // Bolt app setup
